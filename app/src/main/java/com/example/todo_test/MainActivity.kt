1bf1e09e29764d8275cf9f4fa49484fb23e6b6a5
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-23 15:12:48 +0530

    Update MainActivity.kt
    move Greeting to its own file
    Added HomeScreen to the content of app, moving forward routing will be added

--- app/src/main/java/com/example/todo_test/MainActivity.kt
+++ app/src/main/java/com/example/todo_test/MainActivity.kt
@@ -4,18 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.todo_test.screen.HomeScreen
 import com.example.todo_test.ui.theme.TodoTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,57 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoTestTheme {
-                Scaffold { innerPadding ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Greeting(
-                            name = "Ayush Kumar Jaiswal",
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                    }
-                }
+                HomeScreen()
             }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    var count by remember { mutableStateOf(0) }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize()
-    ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-
-        Button(
-            onClick = {count++},
-            modifier = Modifier.
-            padding(top = 10.dp)
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.CenterHorizontally)
-            ,
-            colors = ButtonColors(
-                containerColor = Color.Cyan,
-                contentColor = Color.Blue,
-                disabledContainerColor = Color.Cyan,
-                disabledContentColor = Color.Blue,
-            )
-        ) {
-            Text(
-                text = "Click Me $count",
-            )
         }
     }
 }
