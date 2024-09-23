28d7a5d581314d5d5942fff9c1b3f31716c696c0
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-23 15:09:52 +0530

    Added navigation library for routing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // additional added
+    implementation(libs.androidx.navigation.compose)
 }
