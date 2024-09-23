acc0b66cd899ff14ead7c2c0dd83cb85bf603e35
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-23 15:11:22 +0530

    Create HomeScreen.kt
    render the home page of the app

---
+++ app/src/main/java/com/example/todo_test/screen/HomeScreen.kt
@@ -0,0 +1,42 @@
+package com.example.todo_test.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.todo_test.components.home.Counter
+
+@Composable
+@Preview(showBackground = false)
+fun HomeScreen() {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Box (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(25.dp)
+        ){
+            Row (
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp)
+            ){
+                Text(
+                    text = "Counter Component",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
+            Counter()
+        }
+    }
+}
