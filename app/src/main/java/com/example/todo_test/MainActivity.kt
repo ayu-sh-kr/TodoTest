40719970f8986c95c54280d902546d2bd2f63b91
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-25 00:10:29 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/todo_test/MainActivity.kt
+++ app/src/main/java/com/example/todo_test/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.todo_test.screen.HomeScreen
+import com.example.todo_test.screen.LoginScreen
 import com.example.todo_test.ui.theme.TodoTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TodoTestTheme {
-                HomeScreen()
+                LoginScreen()
             }
         }
     }
