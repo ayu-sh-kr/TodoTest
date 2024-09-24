8327e2a8204bd09b55076a047e03ca847dbe2128
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-25 00:10:26 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/example/todo_test/screen/HomeScreen.kt
+++ app/src/main/java/com/example/todo_test/screen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.todo_test.screen
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -15,6 +16,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.todo_test.components.home.Counter
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 @Preview(showBackground = false)
 fun HomeScreen() {
