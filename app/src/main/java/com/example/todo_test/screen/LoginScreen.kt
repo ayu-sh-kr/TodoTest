ec274eed2022a96cc60b5cbb0b93061344c55445
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-25 23:41:40 +0530

    Update LoginScreen.kt
    Code optimization

--- app/src/main/java/com/example/todo_test/screen/LoginScreen.kt
+++ app/src/main/java/com/example/todo_test/screen/LoginScreen.kt
@@ -2,20 +2,14 @@ package com.example.todo_test.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -32,22 +26,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.todo_test.components.login.InputField
 import com.example.todo_test.ui.theme.Blue400
-import com.example.todo_test.ui.theme.Gray300
 import com.example.todo_test.ui.theme.Gray50
-import com.example.todo_test.ui.theme.Gray600
 import com.example.todo_test.ui.theme.Gray900
-import com.example.todo_test.ui.theme.Red300
 import com.example.todo_test.ui.theme.Red600
 import com.example.todo_test.ui.theme.Slate100
-import com.example.todo_test.ui.theme.Slate400
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -146,69 +135,5 @@ fun LoginScreen() {
             }
 
         }
-    }
-}
-
-@Composable
-fun InputField(
-    value: String,
-    onValueChange: (String) -> Unit,
-    label: String,
-    isPassword: Boolean,
-    placeholder: String
-) {
-    Column(
-        modifier = Modifier.padding(
-            horizontal = 16.dp,
-            vertical = 10.dp
-        )
-    ) {
-        Text(
-            text = label,
-            color = Slate400,
-            fontSize = 18.sp,
-            fontWeight = FontWeight(500),
-            modifier = Modifier.padding(bottom = 4.dp)
-        )
-
-        val interactionSource = remember { MutableInteractionSource() }
-        val isFocused by interactionSource.collectIsFocusedAsState()
-
-
-        BasicTextField(
-            value = value,
-            onValueChange = onValueChange,
-            textStyle = TextStyle(
-                color = Gray900,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Medium
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(color = Gray50, shape = RoundedCornerShape(8.dp))
-                .height(40.dp)
-                .border(
-                    width = if (isFocused) 3.dp else 0.dp,
-                    color = if (isFocused) Red300 else Gray300,
-                    shape = RoundedCornerShape(8.dp),
-                )
-                .padding(10.dp),
-            interactionSource = interactionSource,
-            visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
-            decorationBox = { innerTextField ->
-                Box(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.CenterStart
-                ) {
-                    if (value.isEmpty()) {
-                        Text(
-                            text = placeholder,
-                            color = Gray600
-                        )
-                    }
-                    innerTextField()
-                }
-            }
-        )
     }
 }
