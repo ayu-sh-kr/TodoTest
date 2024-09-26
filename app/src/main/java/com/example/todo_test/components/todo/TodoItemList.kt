bd9a858d82b9bd2a692df076d56923d623313d98
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-27 02:43:12 +0530

    Update TodoItemList.kt
    added vertical scroll in the Todo Item List composable

--- app/src/main/java/com/example/todo_test/components/todo/TodoItemList.kt
+++ app/src/main/java/com/example/todo_test/components/todo/TodoItemList.kt
@@ -2,8 +2,11 @@ package com.example.todo_test.components.todo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
+import androidx.compose.ui.Modifier
 import com.example.todo_test.screen.todo.TodoItem
 
 @Composable
@@ -12,8 +15,11 @@ fun TodoItemList(
     onTodoCheckedChange: (Int, Boolean) -> Unit,
     onTodoDelete: (Int) -> Unit
 ) {
+    val scrollState = rememberScrollState();
     Column(
-        verticalArrangement = Arrangement.SpaceEvenly
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .verticalScroll(scrollState)
     ) {
         for (item in items) {
             key(item.id) {
@@ -21,7 +27,6 @@ fun TodoItemList(
                     item = item,
                     onCheckedChange = {
                         onTodoCheckedChange(item.id, !item.isDone)
-                        println("Check box clicked")
                     },
                     onDeleteClick = {
                         onTodoDelete(item.id)
