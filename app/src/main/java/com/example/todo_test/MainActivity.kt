9dc08c883300370bf0eeaa760e3624e8bb85af2c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-27 02:42:48 +0530

    Update MainActivity.kt
    create database by lazy,
    created viewModel object

--- app/src/main/java/com/example/todo_test/MainActivity.kt
+++ app/src/main/java/com/example/todo_test/MainActivity.kt
@@ -4,16 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.todo_test.screen.TemporaryTodoScreen
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.todo_test.screen.todo.TemporaryTodoScreen
+import com.example.todo_test.screen.todo.TodoItemDatabase
+import com.example.todo_test.screen.todo.TodoItemViewModel
 import com.example.todo_test.ui.theme.TodoTestTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            TodoItemDatabase::class.java,
+            "todo_items.db"
+        ).build()
+    }
+
+    private val viewModel by viewModels<TodoItemViewModel> (
+        factoryProducer = {
+            object: ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return TodoItemViewModel(db.todoItemDao()) as T
+                }
+            }
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TodoTestTheme {
-                TemporaryTodoScreen()
+                TemporaryTodoScreen(viewModel)
             }
         }
     }
